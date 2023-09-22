2a45b4b9e7cb6b402ff7da2cf0a87461a269cd9e
Tyler-Phillips12 <Tyler-Phillips12@github.com>
2023-09-21 22:18:32 -0400

    added list of ingredients

--- app.js
+++ app.js
@@ -45,6 +45,33 @@ function spinSpinner() {
     mealImage.src = selectedRecipe.image;
     mealName.textContent = selectedRecipe.title;
 
+    fetchRecipeDetails(selectedRecipe.id);
+
+    function fetchRecipeDetails(recipeId) {
+      // Make an API request to fetch recipe details by ID
+      fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=8d241ad6e0524291bb07e903cffd0941`)
+        .then(response => response.json())
+        .then(data => {
+          const selectedRecipe = data;
+    
+          // Extract and format ingredients
+          const ingredientsList = selectedRecipe.extendedIngredients.map(ingredient => ingredient.original);
+    
+          // Update the mealRecipe element with ingredients
+          mealRecipe.innerHTML = `
+            <h2>Ingredients:</h2>
+            <ul>
+              ${ingredientsList.map(ingredient => `<li>${ingredient}</li>`).join('')}
+            </ul>
+            <h2>Instructions:</h2>
+            <p>${selectedRecipe.instructions || 'No instructions available.'}</p>
+          `;
+        })
+        .catch(error => {
+          console.error('Error fetching recipe details:', error);
+        });
+    }
+
     mealRecipe.innerHTML = `<p>${selectedRecipe.instructions || 'No instructions available.'}</p>`;
 
     currentIndex++;
